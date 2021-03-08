51687ae3dbeac3a4af609e354e9dbbf9c083c840
Nishi <naalnishinelson@gmail.com>
2021-03-08 12:34:27 -0800

    user auth stuff done

--- express-project-starter-master/app.js
+++ express-project-starter-master/app.js
@@ -38,6 +38,8 @@ store.sync();
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
--- express-project-starter-master/routes/users.js
+++ express-project-starter-master/routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { asyncHandler, csrfProtection } = require('./utils');
+const { check, validationResult } = require('express-validator');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/login', csrfProtection, asyncHandler( async (req, res) =>{
+  res.render('login-form', {
+    csrfToken: req.csrfToken(),
+  });
+}));
 
 module.exports = router;
