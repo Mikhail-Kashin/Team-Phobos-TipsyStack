9f247ad520eb77391fccfcab1dd15fcfaceb34db
Tristan San Juan <tristansj88@gmail.com>
2021-03-09 14:15:22 -0700

    cocktailQ routes and pug file set up

--- express-project-starter-master/app.js
+++ express-project-starter-master/app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const cocktailqRouter = require('./routes/cocktailq')
 const { restoreUser } = require('./auth');
 
 const app = express();
@@ -40,7 +41,7 @@ store.sync();
 app.use(restoreUser);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/Cocktail-Qs', cocktailqRouter)
 
 
 // catch 404 and forward to error handler
---
+++ express-project-starter-master/routes/cocktailq.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const router = express.Router();
+const { asyncHandler, csrfProtection } = require("./utils");
+const { check, validationResult } = require("express-validator");
+const { CocktailQ } = require('../db/models');
+
+
+const cocktailQNotFoundError = (id) => {
+  const err = Error("Cocktail-Q not found");
+  err.errors = [`Cocktail-Q with id of ${id} could not be found.`];
+  err.title = "Cocktail-Q not found.";
+  err.status = 404;
+  return err;
+};
+const cocktailQValidators = [
+   
+    check('question')
+        .exists({ checkFalsy: true })
+        .withMessage('FIELD REQUIRED')
+        
+]
+router.get("/", csrfProtection, asyncHandler(async (req, res) => {
+    const cocktailqs = await CocktailQ.findAll({
+        
+            
+        order: {
+            ascending,
+            createdAt
+            },
+        limit: 10
+        
+    });
+    res.render("cocktail-q", { cocktailqs, csrfToken: req.csrfToken() });
+}))
+router.get('/new', csrfProtection, asyncHandler(async (req, res) => {
+    res.render('cocktailq-question', {csrfToken: csrfToken()})
+}))
+
+
+router.post('/new', csrfProtection, cocktailQValidators, asyncHandler(async (req, res) => {
+    const {
+        question,
+    } = req.body
+    await CocktailQ.create({question})
+    res.redirect('/Cocktail-Q')
+}))
+
+router.get('/id(\\d+)', csrfProtection, asyncHandler(async (req, res) => {
+    const cocktailq = await CocktailQ.findOne({
+        where: {
+            include: CocktailA,
+            id: req.params.id
+        }
+    })
+    res.render('cocktailq-id', { cocktailq, csrfToken: req.csrfToken() })
+}));
+
+
+router.get('/id(\\d+)/edit', csrfProtection, asyncHandler(async (req, res) => {
+    res.render('cocktailq-edit-page', { csrfToken: csrfToken()})
+}))
+
+
+router.post('/id(\\d+)/edit', cocktailQValidators, asyncHandler(async (req, res, next) => {
+    const cocktailq = await CocktailQ.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    if (req.user.id !== cocktailq.userId) {
+        const err = new Error('Access Denied 🚫')
+        err.status = 401
+        err.message = "You do no have sufficient access to edit this Cocktail-Q!"
+        err.title = "Access Denied 🚫"
+        throw err
+    }
+    if (cocktailq) {
+        await cocktailq.update({ question: req.body.question })
+        res.redirect('/')
+        
+    }
+    else {
+        next(cocktailQNotFoundError(req.params.id))
+    }
+}))
+
+// router.delete('/id(\\d+)', asyncHandler(async (req, res, next) => {
+//     const cocktailq = await CocktailQ.findOne({
+//         where: {
+//           id: req.params.id
+//       }
+//     })
+//      if (req.user.id !== cocktailq.userId) {
+//        const err = new Error("Access Denied 🚫");
+//        err.status = 401;
+//        err.message =
+//          "You do no have sufficient access to edit this Cocktail-Q!";
+//        err.title = "Access Denied 🚫";
+//        throw err;
+//      }
+//     if (cocktailq) {
+//         await cocktailq.destroy();
+//         res.render({message: `Deleted Cocktail-Q with id of ${req.params.id}.`})
+//     } else {
+//         next(cocktailQNotFoundError(req.params.id))
+//     }
+// }))
+
+module.exports = router;
