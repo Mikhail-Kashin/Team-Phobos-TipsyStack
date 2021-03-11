7333b081997f95f97976cb85c73a85813fb3ab89
Tristan San Juan <tristansj88@gmail.com>
2021-03-11 14:47:47 -0700

    midway save

--- routes/search.js
+++ routes/search.js
@@ -9,17 +9,30 @@ const { check, validationResult } = require("express-validator");
 const { requireAuth } = require("../auth");
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op;
-const { CocktailQ }= require('../db/models');
+const { CocktailQ, CocktailA, User, Vote } = require('../db/models');
+
+router.get('/search-reasult', csrfProtection, asyncHandler(async (req, res) => {
+    const questions = await CocktailQ.findAll({
+        include: [User, CocktailA, Vote]
+    })
+    res.render('search', {
+        questions,
+        csrfToken: req.csrfToken()
+    })
+}))
 
 router.post('/', csrfProtection, asyncHandler(async (req, res) => {
     const { query } = req.body;
     const questions = await CocktailQ.findAll({
         where: {
             content: {
-                [Op.substring] : query
+                [Op.substring] : `%${query}%`
             }
         }
     })
 
-    res.render()
+    res.render('search', {
+        questions,
+        csrfToken: req.csrfToken()
+    })
 }))
