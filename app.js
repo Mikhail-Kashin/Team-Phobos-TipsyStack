dc06b252d5dc7e4942256dc9f3f96e3d3e52a984
adambailey <arb5433@gmail.com>
2021-03-12 16:50:00 -0500

    “finished cleaning up the code and the files”

--- app.js
+++ app.js
@@ -46,6 +46,8 @@ app.get('*', csrfProtection, (req, res, next) => {
   next()
 })
 app.use(restoreUser);
+
+// setting up our routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/CocktailQs/:qId/CocktailAs', cocktailARouter);
@@ -62,7 +64,6 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
